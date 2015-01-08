911a35b37a0a2ab224f007e32df4b0fd7b72e74d
lazarus  morris <lazarusmorris@ymail.com>
2015-01-08 13:58:26 -0800

    init

---
+++ public_html/script.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$("document").ready(function(){
+    $(".practice").css("background-color", "indigo");
+    $("p span:first-child").css("background-colo","orange");
+    $("div p:last-child").css("background-color","cyan");
+    
+});

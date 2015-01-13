66b54f7aca2376a26b48f2ad9c4942d61901c24b
lazarus  morris <lazarusmorris@ymail.com>
2015-01-13 13:56:11 -0800

    no message

--- public_html/script.js
+++ public_html/script.js
@@ -6,20 +6,65 @@
 
 
 $("document").ready(function(){
-    $(".practice").css("background-color", "indigo");
+    $(".practice").css("background-color", "red");
     $("p span:first-child").css("background-colo","orange");
     $("div p:last-child").css("background-color","cyan");
-    $('#onebutton').bind('click',alertButtonClick);
-    $('#textBox1').bind('blur',onBlurEvent)
-                  .bind('focus',onFocusEvent)
-                  .bind('onmousedown', onMDownEvent)
-                  .bind('onmouseup', onMUpEvent)
-                  .bind('change', onChangeEvent);
+    $('#oneButton').bind('click',alertButtonClick);
+    $('p').bind('mouseover',mouseOverMe).bind('mouseout',mouseOutMe);
+    $('#twoButton').bind('dblclick',dblClickedMe);
+    $('#replaceWText').bind('click',replaceWText);
+    $('#removePara').bind('click',remoAPara);
+   // $("form").submit(function()){alert("Submit button clicked")};
+//    $('#textBox1').bind('blur',onBlurEvent)
+//                  .bind('focus',onFocusEvent)
+//                  .bind('onmousedown', onMDownEvent)
+//                  .bind('onmouseup', onMUpEvent)
+//                  .bind('change', onChangeEvent);
+//          $(window).resize(resizeWindow);
 });
+function removeAPara(){
+    $('#randPara p:last').remove();
+}
+function addParra(){
+    $('randPara').append('<p>ADDED</p>');
+}
+function replaceWText(){
+    $('replaceWtext').text('Replaced!');
+}
 function alertButtonClick(){
     alert("There was a button clicked");
 }
 
-function onBlurEvent(){
-    $("#second").html("You left the box");
+function mouseOverMe(){
+    $(".practice").html("You put your cursor on my logo");
 }
+
+function mouseOutMe(){
+    $("second").html("You stopped touching my logo");
+}
+
+function dblClickedMe(){
+    $("second").html("You double clicked my button");
+}
+
+
+//function onBlurEvent(){
+//    $("#second").html("You left the box");
+//}
+//
+//function onFocusEvent(){
+//    $("#second").html("You left the box");
+//}
+//function onMDownEvent(){
+//    $("#second").html("You left the box");
+//}
+
+//function onMUpEvent(){
+//    $("#second").html("You left the box");
+//}
+//function onChangeEvent(){
+//    $("#second").html("You left the box");
+//}
+//function resizedWindow(){
+//    $("#second").html("Window was resized W:"+$(window).width());
+//}
